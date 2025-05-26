75c875e83e84c7979bd38a42360e26dbb5566375
William <william.jewers@gmail.com>
2025-05-26 12:09:42 +0100

    commit

--- components/FeedCard/components/PostTag/PostTag.js
+++ components/FeedCard/components/PostTag/PostTag.js
@@ -1,19 +1,26 @@
+// PostTag.jsx
 import React, { useContext } from 'react';
 import styles from './PostTag.module.css';
 import { SearchContext } from '../../../../pages/api/SearchContext';
-export default function PostTag({ text }) {
-    const { setQuery } = useContext(SearchContext);
-  
-    const handleClick = () => {
-      setQuery(`${text}`);
-    };
-  
-    return (
-      <button 
-        className={styles.postTag} 
-        onClick={handleClick}
-      >
-        {text}
-      </button>
-    );
-  }
+import clsx from 'clsx';
+
+export default function PostTag({ text, indicator = false }) {
+  const { setQuery } = useContext(SearchContext);
+
+  const handleClick = () => {
+    // don’t trigger a search when clicking the “+N” indicator
+    if (!indicator) setQuery(text);
+  };
+
+  return (
+    <button
+      className={clsx(
+        styles.postTag,
+        indicator && styles.indicatorTag
+      )}
+      onClick={handleClick}
+    >
+      {text}
+    </button>
+  );
+}
