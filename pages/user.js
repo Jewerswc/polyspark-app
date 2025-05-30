50d4ab8e20ad30b9f266b4c262dd075350fd07dc
William <william.jewers@gmail.com>
2025-05-30 16:51:27 +0100

    Update user.js

--- pages/user.js
+++ pages/user.js
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
-import Header from './../components/Header/Header';
-import UserProfile from './../components/User/UserProfile';
+import React, { useState, useEffect } from 'react';
 
+import Header from './../components/Header/Header';
+import HeaderMobile from './../components/Header/HeaderMobile'
+import UserProfile from './../components/User/UserProfile';
+import MobileNavbar from '../components/MobileNavbar/MobileNavbar';
 import Footer from '../components/Footer/Footer';
 import ChatOverlay from '../components/ChatOverlay/ChatOverlay';
 import SignupOverlay from './../components/LoginOverlay/LoginOverlay';
 import './user.module.css';
+
+function useIsMobile(breakpoint = 768) {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const onResize = () => setIsMobile(window.innerWidth < breakpoint);
+    onResize();
+    window.addEventListener('resize', onResize);
+    return () => window.removeEventListener('resize', onResize);
+  }, [breakpoint]);
+  return isMobile;
+}
 
 export default function App() {
   const [isSignupOverlayVisible, setSignupOverlayVisible] = useState(false);
@@ -15,20 +28,33 @@ export default function App() {
   const [isChatOverlayVisible, setChatOverlayVisible] = useState(false);
   const openChatOverlay = () => setChatOverlayVisible(true);
   const closeChatOverlay = () => setChatOverlayVisible(false);
+  const isMobile = useIsMobile();
 
   return (
-    <div className="pageWrapper">
-      {/* Pass openSignupOverlay as a prop */}
-      <Header onSignupClick={openSignupOverlay} />
-      <div className="mainContent">
-        <UserProfile />
-      </div>
-      <Footer />
-
-      {isChatOverlayVisible && (
-        <ChatOverlay onClose={closeChatOverlay} />
-      )}
-
+        <div className={isMobile ? 'pageWrapperMobile' : 'pageWrapper'}>
+          {isMobile ? (
+            <>
+              {/* Mobile header could be a slimmer version, if you have one */}
+              <HeaderMobile onSignupClick={openSignupOverlay} mobile />
+              <main className="mainContentMobile">
+                {/* maybe a mobile-friendly profile card */}
+                <UserProfile mobile />
+              </main>
+                          <MobileNavbar
+             
+                          />
+              {/* mobile nav / overlays */}
+            </>
+          ) : (
+            <>
+              {/* DESKTOP LAYOUT */}
+              <Header onSignupClick={openSignupOverlay} />
+              <main className="mainContent">
+                <UserProfile />
+              </main>
+              <Footer />
+            </>
+          )}
       {isSignupOverlayVisible && (
         <SignupOverlay 
           onGoogleContinue={() => console.log("Google Continue")}
