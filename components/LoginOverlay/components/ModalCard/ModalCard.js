a629e701414b977e1a26c9dd9d99515c6c071778
William <william.jewers@gmail.com>
2025-06-09 11:19:39 +0100

    Commit

--- components/LoginOverlay/components/ModalCard/ModalCard.js
+++ components/LoginOverlay/components/ModalCard/ModalCard.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import styles from './ModalCard.module.css';
+
+export default function ModalCard({ onClose, children }) {
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.card} onClick={e => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>
+  );
+}
  
